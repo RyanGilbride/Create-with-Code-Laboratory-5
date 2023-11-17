12ee012a41ff85855f89e1b4158f2a722e5301a5
RyanGilbride <147630020+RyanGilbride@users.noreply.github.com>
2023-11-17 04:42:10 +0000

    update

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -8,8 +8,7 @@ public class DifficultyButton : MonoBehaviour
     private Button button;
     private GameManager gameManager;
     public int difficulty;
-
-    // Start is called before the first frame update
+    // Start called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
@@ -18,7 +17,7 @@ public class DifficultyButton : MonoBehaviour
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
+    // Update called once per frame
     void Update()
     {
         
